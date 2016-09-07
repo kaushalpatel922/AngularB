37965dffd7e5afbd67bac51c670795839d8a5866
Kaushal Patel <kaushalpatel922@gmail.com>
2016-09-07 15:50:56 -0400

    shared value from controller A to B

--- app.js
+++ app.js
@@ -31,3 +31,21 @@ myApp.controller('OuterCtrl', function($scope) {
 myApp.controller('InnerCtrl', function($scope) {
 
 });
+
+// For ControllerA and ControllerB
+
+myApp.factory('Share', function() {
+  return {sharedMessage: "I am shared"};
+})
+
+myApp.controller('controllerA', function controllerA($scope,Share) {
+  $scope.value = "Hello from controller A";
+  $scope.sharedValue = 'value@A';
+  $scope.sharedValue = Share;
+})
+
+myApp.controller('controllerB', function controllerB($scope,Share) {
+  $scope.value = "Hello from controller B";
+  $scope.sharedValue = 'value@B';
+  $scope.sharedValue = Share;
+})
