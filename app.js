807a9d32a7c2d01e2d27d16363f2bb8b177c0c9a
Kaushal Patel <kaushalpatel922@gmail.com>
2016-09-07 10:56:07 -0400

    divide two numbers-ngClick

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 var myApp = angular.module('myApp', []);
+
+myApp.controller('DivideCtrl', function($scope) {
+  $scope.numberData = {
+    value1: 10,
+    value2: 2
+  }
+
+  $scope.divide = function(val1,val2) {
+    window.alert(val1/val2);
+  }
+})
